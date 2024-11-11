1c492ae2f7884cfff7f3221a8a65bc66e1ab4d71
gyongcode <gyongcode@gmail.com>
2024-11-11 14:49:51 +0900

    feat: 입출력 구현

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -1,2 +1,64 @@
-package store.view;public class InputView {
+package store.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import store.model.Product;
+import store.model.Promotion;
+
+public class InputView {
+
+    public String[] readItem() {
+        System.out.println("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
+        String[] input = Console.readLine().split(",");
+
+        return input;
+    }
+
+    public boolean readApplyPromotion(Product product) {
+        System.out.println("현재 " + product.getName() + "은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)");
+        String input = Console.readLine();
+        validateYN(input);
+        if (input.equals("Y")) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean readNotApplyPromotion(String name, int count) {
+        System.out.println(
+            "현재 " + name +" "+count+"개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)");
+        String input = Console.readLine();
+        validateYN(input);
+
+        if (input.equals("Y")) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean readApplyMembership(){
+        System.out.println("멤버십 할인을 받으시겠습니까? (Y/N)");
+        String input = Console.readLine();
+        validateYN(input);
+
+        if (input.equals("Y")) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean readApplyAgain(){
+        System.out.println("감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)");
+        String input = Console.readLine();
+        validateYN(input);
+
+        if (input.equals("Y")) {
+            return true;
+        }
+        return false;
+    }
+
+    private void validateYN(String input){
+        if(!input.equals("Y") && !input.equals("N"))
+            throw new IllegalArgumentException("[ERROR] 잘못된 입력입니다. 다시 입력해 주세요.");
+    }
 }
--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -1,2 +1,32 @@
-package store.view;public class OutputView {
+package store.view;
+
+import java.util.List;
+import store.model.AmountInformation;
+import store.model.Product;
+import store.model.Receipt;
+
+public class OutputView {
+
+    public void printProducts(List<Product> products) {
+        System.out.println("안녕하세요. W편의점입니다.");
+        System.out.println("현재 보유하고 있는 상품입니다.\n");
+
+        for (Product product : products) {
+            System.out.println(product.toString());
+        }
+    }
+
+    public void printReceipt(Receipt receipt, AmountInformation amountInformation) {
+        System.out.println("==============W 편의점================");
+
+        System.out.println("상품명\t\t수량\t금액");
+        System.out.println(receipt.toStringBuyProducts());
+
+        System.out.println("=============증\t정===============");
+        System.out.println(receipt.toStringPromotionProducts());
+
+        System.out.println("====================================");
+        System.out.println(amountInformation.toString());
+    }
+
 }
